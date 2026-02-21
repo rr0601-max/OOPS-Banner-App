efe33227fe16cb96cec488f35675b08520c9366e
Developer <dev@oops.com>
2026-02-21 16:00:52 +0530

    Initial commit: OOPS Banner App project setup

---
+++ OOPSBannerApp.java
@@ -0,0 +1,24 @@
+/**
+ * OOPSBannerApp - A simple application to display OOPS
+ * 
+ * This application demonstrates basic Java concepts including:
+ * - Class structure
+ * - Main method as entry point
+ * - Console output using System.out.println
+ * - String literals
+ * 
+ * @author YourName
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+    
+    /**
+     * Main method - Entry point of the application
+     * 
+     * @param args Command line arguments (not used in this version)
+     */
+    public static void main(String[] args) {
+        // Print OOPS to console
+        System.out.println("OOPS");
+    }
+}
