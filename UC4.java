9336b2bc1cb606b31c94d447d8e63d05e178b576
Developer <dev@oops.com>
2026-02-21 17:59:23 +0530

    added UC4

--- UC4.java
+++ UC4.java
@@ -1,16 +1,4 @@
-/**
- * UC4 - OOPS Banner App (Array & Loops)
- *
- * Stores banner lines in an array and prints them using an enhanced for-loop.
- * Each line is constructed with String.join() to avoid repeated concatenation.
- *
- * Compilation: javac UC4.java
- * Execution:   java UC4
- *
- * @author Developer
- * @version 1.0
- * @since 2026-02-21
- */
+
 public class UC4 {
     public static void main(String[] args) {
         String[] O = {
